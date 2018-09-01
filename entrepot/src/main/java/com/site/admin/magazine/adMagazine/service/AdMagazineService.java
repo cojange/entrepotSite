18a2a5b65b7c51bfe2326b1136d148ceea2095bc
ShinHyeju <ShinHyeju@DESKTOP-8392M6E>
2018-09-01 11:22:26 +0900

    상품 등록 작업중

--- entrepot/src/main/java/com/site/admin/magazine/adMagazine/service/AdMagazineService.java
+++ entrepot/src/main/java/com/site/admin/magazine/adMagazine/service/AdMagazineService.java
@@ -6,6 +6,6 @@ import com.site.client.magazine.ditail.vo.MagazineVO;
 
 public interface AdMagazineService {
 
-	public List<MagazineVO> adMagazineList(MagazineVO mvo);
+	public List<MagazineVO> adMagazineList(MagazineVO mgvo);
 
 }
